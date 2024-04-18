eede36de73523bf3cd4bc5b481e5bb32143c3f2a
Kate Sills <katelynsills@gmail.com>
2024-04-18 10:27:53 -0700

    remove outdated gatsby-plugin-offline npm package that has lodash vulnerabilities

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,8 +121,5 @@ module.exports = {
         icon: `static/favicon/android-chrome-512x512.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
