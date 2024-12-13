296eac5815f8db1e5b1414c8a8e856199d0c9487
Kate Sills <katelynsills@gmail.com>
2024-12-13 13:25:24 -0800

    Add jan meeting author reading (#21)
    * add jan meeting
    
    * add author reading

--- src/pages/meetings.js
+++ src/pages/meetings.js
@@ -10,26 +10,36 @@ const MeetingsPage = ({ location, data }) => {
     <Layout location={location} title={siteTitle}>
       <h1>Meetings</h1>
       <p>
-        We meet on Second Saturdays from 10:30am to 12:30pm for a friendly peer
-        critique. Occasionally, our meeting date changes due to room scheduling
-        conflicts. See the{" "}
-        <a href="#meeting-dates">meeting dates for 2024 below</a>.
+        We meet on Second Saturdays from 10:30am to 12:30pm to share our writing
+        and get friendly feedback. Occasionally, our meeting date changes due to
+        room scheduling conflicts. See the{" "}
+        <a href="#meeting-dates">meeting dates for 2025 below</a>.
       </p>
       <p>
-      <b>Jamestown Community Hall</b>
-      <br /> 18250 Main St.
-      <br /> Jamestown, CA 95327
-      <br />
-      <a href="https://maps.app.goo.gl/RX6uZ3Fy43uXdT8e8">Google Maps</a>
+        <b>Note: This is our new meeting location as of Nov 2024!</b>
+        <br />
+        23685 Marble Quarry Rd
+        <br />
+        Columbia, CA 95310
+        <br />
+        <a href="https://maps.app.goo.gl/qwdtyF9ViTyyPAmn9">Google Maps</a>
+      </p>
+      <p>
+        <b>Directions</b>
+        <br />
+        Take Parrotts Ferry Rd to Marble Quarry Road, make a right into the Gold
+        Springs Estates complex. Stay on the road as it turns to the right&
+        watch for Club House on your left. Parking in front, to the left of the
+        building & another lot across the street.
       </p>
       <p>
         {" "}
         If you'd like that all-important feedback, then bring no more than 4
         pages, 12 pt font, double-spaced, 10 copies.
       </p>
-      <h2 id="meeting-dates">Upcoming 2024 Dates</h2>
+      <h2 id="meeting-dates">Upcoming 2025 Dates</h2>
       <ul>
-        <li>Saturday, December 7, 2024</li>
+        <li>Saturday, January 11, 2024</li>
       </ul>
       <iframe
         id="googlecalendar"
