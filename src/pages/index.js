b7d7133361f79d0257792d812e4316074fcdb96f
Kate Sills <katelynsills@gmail.com>
2026-02-28 10:33:32 -0800

    Add cowboy day flyers and event. Update index and meeting pages (#27)

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,32 +10,6 @@ const HomePage = ({ location, data }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`;
   return (
     <Layout location={location} title={siteTitle}>
-      <img width="100%" src={bookAd}></img>
-      <button>
-        <a href="https://www.lulu.com/shop/amylee-and-sara-dykman-and-blanche-abrams-and-kate-sills/when-rain-will-come/paperback/product-84n7ygd.html?page=1&pageSize=4">
-          BUY NOW
-        </a>
-      </button>
-      <h1>When Rain Will Come</h1>
-      <p>
-        Whether you crave the piercing insight of a personal essay, the allure
-        of a well-told tale, eye-catching illustrations, or the timeless beauty
-        of poetry, <i>When Rain Will Come</i> offers a journey through both the
-        ordinary and extraordinary as told by writers and poets of the
-        California Gold Country. Perfect for readers who cherish deep reflection
-        and literary exploration, this collection invites you to discover,
-        reflect, and return again and again.
-      </p>
-      <p>
-        <i>When Rain Will Come</i> was released on November 1st, 2024. Grab your
-        copy from the Lulu Bookstore today!
-      </p>
-      <button>
-        <a href="https://www.lulu.com/shop/amylee-and-sara-dykman-and-blanche-abrams-and-kate-sills/when-rain-will-come/paperback/product-84n7ygd.html?page=1&pageSize=4">
-          GET YOUR COPY
-        </a>
-      </button>
-      <br />
       <About></About>
     </Layout>
   );
--- src/pages/meetings.js
+++ src/pages/meetings.js
@@ -12,35 +12,13 @@ const MeetingsPage = ({ location, data }) => {
       <p>
         We meet on Second Saturdays from 10:30am to 12:30pm to share our writing
         and get friendly feedback. Occasionally, our meeting date changes due to
-        room scheduling conflicts. See the{" "}
-        <a href="#meeting-dates">meeting dates for 2025 below</a>.
-      </p>
-      <p>
-        <b>Note: This is our new meeting location as of Nov 2024!</b>
-        <br />
-        23685 Marble Quarry Rd
-        <br />
-        Columbia, CA 95310
-        <br />
-        <a href="https://maps.app.goo.gl/qwdtyF9ViTyyPAmn9">Google Maps</a>
-      </p>
-      <p>
-        <b>Directions</b>
-        <br />
-        Take Parrotts Ferry Rd to Marble Quarry Road, make a right into the Gold
-        Springs Estates complex. Stay on the road as it turns to the right&
-        watch for Club House on your left. Parking in front, to the left of the
-        building & another lot across the street.
+        room scheduling conflicts. 
       </p>
       <p>
         {" "}
         If you'd like that all-important feedback, then bring no more than 4
         pages, 12 pt font, double-spaced, 10 copies.
       </p>
-      <h2 id="meeting-dates">Upcoming 2025 Dates</h2>
-      <ul>
-        <li>Saturday, April 12th, 2025</li>
-      </ul>
       <iframe
         id="googlecalendar"
         src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%23ffffff&showNav=0&mode=AGENDA&showDate=0&showTz=0&showPrint=0&showTabs=0&showCalendars=0&src=NDUzN2Y5NWEwZjFhZjdkMjZkMDk2NjcxYWU0N2QxNjQzMmI4YzI4MGJlYjhmZTc3M2Y3ZTUxNTBiZjk0MWE2NkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23E4C441"
