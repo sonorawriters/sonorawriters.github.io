af86e614cc1ac8a97e9e266735e71ead569ecb65
Kate Sills <katelynsills@gmail.com>
2026-03-13 19:46:02 -0700

    add meeting info and poetry and prose crawl

--- src/pages/meetings.js
+++ src/pages/meetings.js
@@ -12,12 +12,106 @@ const MeetingsPage = ({ location, data }) => {
       <p>
         We meet on Second Saturdays from 10:30am to 12:30pm to share our writing
         and get friendly feedback. Occasionally, our meeting date changes due to
-        room scheduling conflicts. 
+        room scheduling conflicts.
       </p>
       <p>
         {" "}
         If you'd like that all-important feedback, then bring no more than 4
         pages, 12 pt font, double-spaced, 10 copies.
+      </p>
+      <b>
+        <span class="c5">Saturday April 11</span>
+        <span class="c3">th</span>
+        <span class="c5">&nbsp;10:30-12:30</span>
+      </b>
+      <p>
+        <span>Gold Spring Townhomes Club House</span>
+        <br/>
+        <span class="c2">
+          23685 Marble Quarry Road, Columbia, CA 95310
+        </span>
+      </p>
+      <b>
+        <span class="c5">Saturday May 9</span>
+        <span class="c3">th</span>
+        <span class="c0">&nbsp;10:30-12:30 </span>
+      </b>
+      <p>
+        <span>Goodness Caf&eacute;,1 S. Washington St</span>
+        <span class="c5">.,</span>
+        <span class="c2">&nbsp;Sonora, CA 95370</span>
+      </p>
+      <b>
+        <span class="c5">Saturday June 13</span>
+        <span class="c3">th </span>
+        <span class="c0">10:30-12:30</span>
+      </b>
+      <p>
+        <span>Goodness Caf&eacute;,1 S. Washington St., Sonora, CA 95370</span>
+      </p>
+      <b>
+        <span class="c5">Saturday July 11</span>
+        <span class="c3">th</span>
+        <span class="c5">&nbsp;10:30-12:30,</span>
+      </b>
+      <p>
+        <span class="c2">
+          Gold Springs Townhomes Club House, 23685 Marble Quarry Road, Columbia,
+          CA 95370
+        </span>
+      </p>
+      <b>
+        <span class="c5">Saturday August 8</span>
+        <span class="c3">th</span>
+        <span class="c0">&nbsp;10:30-12:30</span>
+      </b>
+      <p>
+        <span class="c2">
+          Goodness Caf&eacute;, 1 S. Washington St., Sonora, CA 95370
+        </span>
+      </p>
+      <b>
+        <span class="c5">Saturday September 12</span>
+        <span class="c3">th</span>
+        <span class="c0">&nbsp;10:30-12:30</span>
+      </b>
+      <p>
+        <span class="c2">
+          Goodness Caf&eacute;,1 S. Washington St., Sonora, CA 95370
+        </span>
+      </p>
+      <b>
+        <span class="c5">Saturday, October 10</span>
+        <span class="c3">th</span>
+        <span class="c0">, 10:30-12:30</span>
+      </b>
+      <p>
+        <span class="c2">
+          Goodness Caf&eacute;, 1 S. Washington St., Sonora, CA 95370
+        </span>
+      </p>
+      <b>
+        <span class="c5">Saturday, November 14</span>
+        <span class="c3">th</span>
+        <span class="c0">, 10:30-12:30</span>
+      </b>
+      <p>
+        <span class="c2">
+          Goodness Caf&eacute;, 1 S. Washington St., Sonora, CA 95370
+        </span>
+      </p>
+      <b>
+        <span class="c5">Saturday, December 12</span>
+        <span class="c3">th</span>
+        <span class="c5">, 10:30-12:30</span>
+      </b>
+      <p>
+        <span class="c2">
+          Goodness Caf&eacute;, 1 S. Washington St., Sonora, CA 95370
+        </span>
+      </p>
+      <p class="c1">
+        <span class="c0"></span>
       </p>
       <iframe
         id="googlecalendar"
